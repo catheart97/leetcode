20f3173f48355de52c33056e8ad02654a17021d5
Ronja Schnur <catheart97@outlook.com>
2022-09-08 10:52:00 +0200

    :sparkles: Added 0094 recursive approach.

--- 0094 - Binary Tree Inorder Traversal.cpp
+++ 0094 - Binary Tree Inorder Traversal.cpp
@@ -15,35 +15,59 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// iterative solution
+
+// class Solution
+// {
+// public:
+//     std::vector<int> inorderTraversal(TreeNode *root)
+//     {
+//         std::vector<int> result;
+//         if (!root) return std::move(result);
+
+//         std::stack<TreeNode *> nodes{};
+//         std::unordered_set<TreeNode *> visited;
+//         nodes.push(root);
+        
+//         while (!nodes.empty())
+//         {
+//             auto node = nodes.top();
+            
+//             if (node->left && visited.find(node->left) == visited.end())
+//             {
+//                 nodes.push(node->left);
+//                 continue;
+//             }
+            
+//             nodes.pop();
+            
+//             result.push_back(node->val);
+//             if (node->right) nodes.push(node->right);
+//             visited.insert(node);
+//         }
+
+//         return std::move(result);
+//     }
+// };
+
+
+// recursive solution (obviously faster)
 class Solution
 {
+    void traverse(TreeNode * node, std::vector<int> & result)
+    {
+        if (!node) return;
+        
+        traverse(node->left, result);
+        result.push_back(node->val);
+        traverse(node->right, result);
+    }
+    
 public:
     std::vector<int> inorderTraversal(TreeNode *root)
     {
         std::vector<int> result;
-        if (!root) return std::move(result);
-
-        std::stack<TreeNode *> nodes{};
-        std::unordered_set<TreeNode *> visited;
-        nodes.push(root);
-        
-        while (!nodes.empty())
-        {
-            auto node = nodes.top();
-            
-            if (node->left && visited.find(node->left) == visited.end())
-            {
-                nodes.push(node->left);
-                continue;
-            }
-            
-            nodes.pop();
-            
-            result.push_back(node->val);
-            if (node->right) nodes.push(node->right);
-            visited.insert(node);
-        }
-
+        traverse(root, result);
         return std::move(result);
     }
 };
